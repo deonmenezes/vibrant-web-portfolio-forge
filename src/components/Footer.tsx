c321ad3a7a112ea10bd876de1f7707a535dec658
deonmenezes <150112885+deonmenezes@users.noreply.github.com>
2025-05-25 12:45:49 +0530

    ff

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -79,8 +79,16 @@ export const Footer = () => {
                 <span>+918104796542</span>
               </li>
               <li className="flex items-center text-gray-300">
+                <Phone className="w-5 h-5 mr-3 text-primary" />
+                <span>+919167342135</span>
+              </li>
+              <li className="flex items-center text-gray-300">
                 <Mail className="w-5 h-5 mr-3 text-primary" />
                 <span>deonmenezescodes@gmail.com</span>
+              </li>
+               <li className="flex items-center text-gray-300">
+                <Mail className="w-5 h-5 mr-3 text-primary" />
+                <span>floyadelys@gmail.com</span>
               </li>
               <li className="flex items-center text-gray-300">
                 <MessageSquare className="w-5 h-5 mr-3 text-primary" />
